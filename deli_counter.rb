ee562f7a6bf525db6ca443810a278aaf0ca07dfd
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2020-12-14 23:56:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,5 +19,10 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+  end
+  katz_deli.shift
 end
