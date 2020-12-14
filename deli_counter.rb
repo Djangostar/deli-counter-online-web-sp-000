c11a1e939d119f98f528aaf88b54380ef6d7c9d4
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2020-12-14 23:33:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,13 @@ require 'pry'
 katz_deli = []
 
 def line(katz_deli)
-
-end
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    katz_deli.map.with_index(1) do
+      |name, num| puts "The line is currently: #{num}. #{name}"
+    end
+  end
 
 def take_a_number(katz_deli, name)
 
