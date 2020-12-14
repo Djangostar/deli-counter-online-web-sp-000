c91edd781ac32151cdb24a13e2521c49a399c224
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2020-12-14 23:26:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,26 +2,13 @@ require 'pry'
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli.empty?
-    puts "The line is currently empty."
-  else
-    line = "The line is currently:"
-    katz_deli.map.with_index(1) do
-      |name, index| line << " #{index}. #{name}"
-    end
-    puts line
-  end
+
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+  
 end
 
 def now_serving(katz_deli)
-  if katz_deli.empty?
-    puts "There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{katz_deli.shift}."
-  end
+
 end
